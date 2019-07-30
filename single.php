500868784cefde681063de2998138811eeb01395
Erickescare <erickdaniel.escareo@gmail.com>
2019-07-30 12:50:11 -0500

    Create single.php

---
+++ single.php
@@ -0,0 +1,136 @@
+<?php $pageTitle = 'Titulo'; include('inc/head.php') ?>
+	<!-- end::Head -->
+	<!-- begin::Body -->
+	<body class="kt-page--loading-enabled kt-page--loading kt-page--fixed kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header--minimize-topbar kt-header-mobile--fixed kt-subheader--enabled kt-subheader--transparent kt-page--loading">
+		<!-- begin:: Page -->
+		<!-- begin:: Header Mobile 
+		<?php include('inc/header-mobile.php') ?>
+		--><!-- end:: Header Mobile -->
+		<div class="kt-grid kt-grid--hor kt-grid--root">
+			<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
+				<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper " id="kt_wrapper">
+					<!-- begin:: Header -->
+					<?php include('inc/header.php') ?>
+					<!-- end:: Header -->
+					<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-grid--stretch">
+						<div class="kt-container kt-body  kt-grid kt-grid--ver" id="kt_body">
+							<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor">
+								<!-- begin:: Content Head -->
+								<div class="kt-subheader   kt-grid__item" id="kt_subheader">
+									<div class="kt-subheader__main">
+										<h3 class="kt-subheader__title">Introducing New Feature</h3>
+										<span class="kt-subheader__separator kt-subheader__separator--v"></span>
+										<span class="kt-subheader__desc">Recientes</span>
+										<!-- <a href="#" class="btn btn-label-primary btn-bold btn-icon-h kt-margin-l-10">
+											Más
+										</a> -->
+									</div>
+								</div>
+								<!-- end:: Content Head -->
+									<!-- begin:: Content -->
+									<div class="kt-content  kt-grid__item kt-grid__item--fluid" id="kt_content">
+										<div class="row">
+											<div class="col-xl-9">
+											<!--begin::Portlet-->
+												<div class="kt-portlet kt-portlet--height-fluid kt-widget19">
+												<div class="kt-portlet__body kt-portlet__body--fit kt-portlet__body--unfill">
+													<div class="kt-widget19__pic kt-portlet-fit--top kt-portlet-fit--sides" style="min-height: 300px; background-image: url(./assets/media//blog/blog3.jpg)">
+														<h3 class="kt-widget19__title kt-font-light">
+															Introducing New Feature
+														</h3>
+														<div class="kt-widget19__shadow"></div>
+														<div class="kt-widget19__labels">
+															<a href="#" class="btn btn-label-light-o2 btn-bold btn-sm ">Recent</a>
+														</div>
+													</div>
+												</div>
+												<div class="kt-portlet__body">
+													<div class="kt-widget19__wrapper">
+														<div class="kt-widget19__content">
+															<div class="kt-widget19__userpic">
+																<img src="./assets/media//users/user1.jpg" alt="">
+															</div>
+															<div class="kt-widget19__info">
+																<a href="#" class="kt-widget19__username">
+																	Anna Krox
+																</a>
+																<span class="kt-widget19__time">
+																	UX/UI Designer, Google
+																</span>
+															</div>
+															<div class="kt-widget19__stats">
+																<span class="kt-widget19__number kt-font-brand">
+																	30
+																</span>
+																<a href="#" class="kt-widget19__comment">
+																	Julio
+																</a>
+															</div>
+														</div>
+														<div class="kt-widget19__text">
+															<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis volutpat velit, ut faucibus diam feugiat et. Praesent eleifend id orci non accumsan. Cras aliquet ligula vitae risus varius, quis aliquam mauris mollis. Proin et odio eleifend, aliquet urna at, mollis tortor. Vivamus congue vehicula sodales. Ut ut eros nulla. Vivamus viverra posuere diam nec condimentum. Sed ornare eu ante eget molestie. Vestibulum enim arcu, pulvinar et ultrices id, dapibus in elit. In sit amet malesuada eros. Praesent pharetra ante id elit volutpat, ac condimentum ipsum congue. Suspendisse imperdiet efficitur feugiat. Morbi sagittis cursus magna, fermentum lobortis nisl. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+															<br>
+															<p>Maecenas scelerisque commodo finibus. Fusce auctor tempus lorem, ac auctor mauris ultrices ut. Fusce quis varius quam, sed mollis turpis. Integer sed dapibus odio. Donec lorem nisi, molestie tincidunt cursus ut, viverra at lorem. Praesent dui ipsum, varius nec faucibus eget, faucibus ut eros. Morbi consectetur neque volutpat turpis congue, finibus placerat quam fringilla. Praesent ac dolor et augue bibendum mollis.</p>
+															<br>
+															<p>Nam aliquam gravida quam at dictum. Duis ornare nibh ut ex facilisis pellentesque. Cras et dolor sed purus feugiat semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent sit amet bibendum nunc, sit amet vulputate sem. Vivamus elit metus, lacinia quis convallis eu, semper et enim. Integer in ligula quam. Curabitur eu urna vitae mauris varius posuere.</p>
+															<br>
+															<p>Nulla pellentesque maximus nisi in vulputate. Sed semper, orci vestibulum pharetra elementum, mi ligula tempus mauris, in cursus felis enim mollis nisi. Praesent volutpat dignissim egestas. Nullam sed elit hendrerit, vehicula orci ac, mattis augue. Mauris at dapibus lectus, id cursus quam. Nunc vel est sed erat tincidunt posuere. Phasellus in egestas sapien. Mauris consequat, lacus sed fringilla dictum, purus orci finibus diam, et hendrerit urna mi vel turpis. Quisque a eros lacinia, accumsan orci ut, laoreet diam. Vivamus non sem vitae nulla imperdiet suscipit sed in libero. Nulla ut placerat quam. Ut lacinia risus in libero gravida, quis vestibulum felis mattis.</p>
+															<br>
+															<p>Nam tristique, nunc pulvinar ultricies maximus, lacus erat tincidunt nulla, sed feugiat dui nunc vitae tortor. Aenean venenatis lectus rutrum, efficitur sem id, gravida lacus. Aliquam sollicitudin porttitor enim id semper. Aenean posuere nulla at ultricies elementum. In iaculis id felis eu tincidunt. Ut interdum leo sit amet nisl sodales vehicula vel ut urna. Integer aliquet hendrerit sapien, nec luctus lorem iaculis eu. Donec sollicitudin massa eget est luctus porttitor. Nunc ac faucibus erat. Duis velit nibh, feugiat vitae orci eu, posuere tempor tellus. Fusce vel velit vitae arcu consectetur iaculis a varius nisi. Etiam feugiat eros in sodales dignissim. Praesent aliquam, risus eu varius fermentum, tellus ligula suscipit nisi, ac lacinia dui ex venenatis leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+														</div>
+													</div>
+												</div>
+											</div>
+												<!--end::Portlet-->
+											</div>
+											<div class="col-xl-3">
+											Side-Bar
+											</div>
+										</div>
+									</div>
+
+									<!-- end:: Content -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+	<!-- begin:: Footer -->
+	<?php include('inc/footer.php') ?>
+	<!-- end:: Footer -->			
+	</div>
+	<!-- end:: Page -->
+	<?php include('inc/quick-panel.php') ?>
+	<!-- begin::Scrolltop -->
+	<div id="kt_scrolltop" class="kt-scrolltop">
+	<i class="fa fa-arrow-up"></i>
+	</div>
+	<!-- end::Scrolltop -->
+	<!-- begin::Global Config(global config for global JS sciprts) -->
+	<!-- end::Global Config -->
+	<!--begin:: Global Mandatory Vendors -->
+	<?php include('inc/mansoryjs.php') ?>
+	<!--end:: Global Mandatory Vendors -->
+	<?php include('inc/global-js.php') ?>
+	<!--end:: Global Optional Vendors -->
+	<!--begin::Global Theme Bundle(used by all pages) -->
+	<script src="./assets/js/demo1/scripts.bundle.js" type="text/javascript"></script>
+	<!--end::Global Theme Bundle -->
+	<!--begin::Page Vendors(used by this page) -->
+	<script src="./assets/vendors/custom/datatables/datatables.bundle.js" type="text/javascript"></script>
+	<!--end::Page Vendors -->
+	<!--begin::Page Vendors(used by this page) -->
+	<script src="./assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+	<script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM" type="text/javascript"></script>
+	<script src="./assets/vendors/custom/gmaps/gmaps.js" type="text/javascript"></script>
+	<!--end::Page Vendors -->
+	<!--begin::Page Scripts(used by this page) -->
+	<script src="./assets/js/demo1/pages/crud/datatables/basic/basic.js" type="text/javascript"></script>
+	<!--end::Page Scripts -->
+	<!--begin::Page Scripts(used by this page) -->
+	<script src="./assets/js/demo1/pages/dashboard.js" type="text/javascript"></script>
+	<!--end::Page Scripts -->
+	</body>
+	<!-- end::Body -->
+</html>
