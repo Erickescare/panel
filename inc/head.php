0a2fc29fa2d5e4f84fff274606888c3313f0b1c4
Erickescare <erickdaniel.escareo@gmail.com>
2019-08-15 11:40:55 -0500

    Sbrs

--- inc/head.php
+++ inc/head.php
@@ -3,7 +3,7 @@
 	<!-- begin::Head -->
 	<head>
 		<!--begin::Base Path -->
-		<base href="./">
+		<base href="/">
 		<!--end::Base Path -->
 		<meta charset="utf-8" />
 		<title><?php echo $pageTitle ?> | Basic Examples</title>
