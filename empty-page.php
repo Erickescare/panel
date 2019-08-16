fbf701269087d3af2d08babe4f38fb6070710e5e
Erickescare <erickdaniel.escareo@gmail.com>
2019-08-16 16:33:09 -0500

    asd

--- empty-page.php
+++ empty-page.php
@@ -3,9 +3,9 @@
 	<!-- begin::Body -->
 	<body class="kt-page--loading-enabled kt-page--loading kt-page--fixed kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header--minimize-topbar kt-header-mobile--fixed kt-subheader--enabled kt-subheader--transparent kt-page--loading">
 		<!-- begin:: Page -->
-		<!-- begin:: Header Mobile 
+		<!-- begin:: Header Mobile -->
 		<?php include('inc/header-mobile.php') ?>
-		--><!-- end:: Header Mobile -->
+		<!-- end:: Header Mobile -->
 		<div class="kt-grid kt-grid--hor kt-grid--root">
 			<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
 				<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper " id="kt_wrapper">
--- inc/header-mobile.php
+++ inc/header-mobile.php
@@ -1,11 +1,67 @@
-<div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed ">
+<div id="kt_header_mobile" class="kt-header__brand kt-header-mobile  kt-header-mobile--fixed ">
 	<div class="kt-header-mobile__logo">
-		<a href="./index.html">
+		<a href="./index.php">
 			<img alt="Logo" src="./assets/media/protexa/ico57.png" />
 		</a>
 	</div>
+	<div class="kt-header__brand-nav">
+		<div class="dropdown">
+			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+				<?php echo "$pageTitle"; ?>
+			</button>
+			<div class="dropdown-menu dropdown-menu-md">
+				<ul class="kt-nav kt-nav--bold kt-nav--md-space">
+					<li class="kt-nav__item">
+						<a class="kt-nav__link active" href="./panel-rh.php">
+							<span class="kt-nav__link-icon"><i class="flaticon2-user"></i></span>
+							<span class="kt-nav__link-text">Recursos Humanos</span>
+						</a>
+					</li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link active" href="./soporte.php">
+							<span class="kt-nav__link-icon"><i class="flaticon-letter-g"></i></span>
+							<span class="kt-nav__link-text">Soporte Google</span>
+						</a>
+					</li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link" href="#">
+							<span class="kt-nav__link-icon"><i class="flaticon-feed"></i></span>
+							<span class="kt-nav__link-text">Relación con el cliente</span>
+						</a>
+					</li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link" href="#">
+							<span class="kt-nav__link-icon"><i class="flaticon2-settings"></i></span>
+							<span class="kt-nav__link-text">Compras</span>
+						</a>
+					</li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link" href="#">
+							<span class="kt-nav__link-icon"><i class="flaticon2-chart2"></i></span>
+							<span class="kt-nav__link-text">Contabilidad</span>
+						</a>
+					</li>
+					<li class="kt-nav__separator"></li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link" href="#">
+							<span class="kt-nav__link-icon"><i class="flaticon-security"></i></span>
+							<span class="kt-nav__link-text">Financiar</span>
+						</a>
+					</li>
+					<li class="kt-nav__item">
+						<a class="kt-nav__link" href="#">
+							<span class="kt-nav__link-icon"><i class="flaticon2-cup"></i></span>
+							<span class="kt-nav__link-text">Administración</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
 	<div class="kt-header-mobile__toolbar">
 		<button class="kt-header-mobile__toolbar-toggler" id="kt_header_mobile_toggler"><span></span></button>
 		<button class="kt-header-mobile__toolbar-topbar-toggler" id="kt_header_mobile_topbar_toggler"><i class="flaticon-more-1"></i></button>
 	</div>
 </div>
+
