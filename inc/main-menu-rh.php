246d550c22fe6607c4ed82fffc8225f715fdc57d
Erickescare <erickdaniel.escareo@gmail.com>
2019-08-16 13:44:56 -0500

    asd

--- inc/main-menu-rh.php
+++ inc/main-menu-rh.php
@@ -14,60 +14,6 @@
 			<li class="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel <?= ($activePage == 'irectorio') ? 'kt-menu__item--here':''; ?>"><a href="./propuestas.php" class="kt-menu__link"><span class="kt-menu__link-text">Directorio</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a></li>
 		</ul>
 	</div>
-	<div class="kt-header__brand-nav">
-		<div class="dropdown">
-			<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-				Principal
-			</button>
-			<div class="dropdown-menu dropdown-menu-md">
-				<ul class="kt-nav kt-nav--bold kt-nav--md-space">
-					<li class="kt-nav__item">
-						<a class="kt-nav__link active" href="./panel-rh.php">
-							<span class="kt-nav__link-icon"><i class="flaticon2-user"></i></span>
-							<span class="kt-nav__link-text">Recursos Humanos</span>
-						</a>
-					</li>
-					<li class="kt-nav__item">
-						<a class="kt-nav__link active" href="./soporte.php">
-							<span class="kt-nav__link-icon"><i class="flaticon-letter-g"></i></span>
-							<span class="kt-nav__link-text">Soporte Google</span>
-						</a>
-					</li>
-					<li class="kt-nav__item">
-						<a class="kt-nav__link" href="#">
-							<span class="kt-nav__link-icon"><i class="flaticon-feed"></i></span>
-							<span class="kt-nav__link-text">Relación con el cliente</span>
-						</a>
-					</li>
-					<li class="kt-nav__item">
-						<a class="kt-nav__link" href="#">
-							<span class="kt-nav__link-icon"><i class="flaticon2-settings"></i></span>
-							<span class="kt-nav__link-text">Compras</span>
-						</a>
-					</li>
-					<li class="kt-nav__item">
-						<a class="kt-nav__link" href="#">
-							<span class="kt-nav__link-icon"><i class="flaticon2-chart2"></i></span>
-							<span class="kt-nav__link-text">Contabilidad</span>
-						</a>
-					</li>
-					<li class="kt-nav__separator"></li>
-					<li class="kt-nav__item">
-						<a class="kt-nav__link" href="#">
-							<span class="kt-nav__link-icon"><i class="flaticon-security"></i></span>
-							<span class="kt-nav__link-text">Financiar</span>
-						</a>
-					</li>
-					<li class="kt-nav__item">
-						<a class="kt-nav__link" href="#">
-							<span class="kt-nav__link-icon"><i class="flaticon2-cup"></i></span>
-							<span class="kt-nav__link-text">Administración</span>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
 	<div class="kt-header-toolbar">
 		<div class="kt-quick-search" id="kt_quick_search_default">
 			<form method="get" class="kt-quick-search__form">
