847f2297722dd91305b91aaeb9752ef76c875203
Erickescare <erickdaniel.escareo@gmail.com>
2019-11-12 16:27:47 -0600

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,56 +1,5 @@
 <!-- begin:: Footer -->
 <div class="kt-footer  kt-footer--extended  kt-grid__item" id="kt_footer">
-	<!-- <div class="kt-footer__top">
-		<div class="kt-container">
-			<div class="row">
-				<div class="col-lg-4">
-					<div class="kt-footer__section">
-						<h3 class="kt-footer__title">TI Protexa</h3>
-						<div class="kt-footer__content">
-							Whatever method you chosse,these nine will
-							make you re-think theway you craft healines
-							Why? Because they all have points in that are
-							worth from a point of view.
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="kt-footer__section">
-						<h3 class="kt-footer__title">Enlaces Interesantes</h3>
-						<div class="kt-footer__content">
-							<div class="kt-footer__nav">
-								<div class="kt-footer__nav-section">
-									<a href="#">Reportes Generales</a>
-									<a href="#">Dashboard Principal</a>
-									<a href="#">Mi Pefil</a>
-								</div>
-								<div class="kt-footer__nav-section">
-									<a href="#">Configuración</a>
-									<a href="#">Chat</a>
-									<a href="#">Custom Reportes</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-4">
-					<div class="kt-footer__section">
-						<h3 class="kt-footer__title">Contacto Rápido</h3>
-						<div class="kt-footer__content">
-							<form action="" class="kt-footer__subscribe">
-								<div class="input-group">
-									<input type="text" class="form-control" placeholder="Email">
-									<div class="input-group-append">
-										<button class="btn btn-brand" type="button">Contacto</button>
-									</div>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div> -->
 	<div class="kt-footer__bottom">
 		<div class="kt-container">
 			<div class="kt-footer__wrapper">
