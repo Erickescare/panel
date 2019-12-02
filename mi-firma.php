1d4bf1598b5cde1125e374403cc4a74657b57c1d
Erickescare <erickdaniel.escareo@gmail.com>
2019-12-02 16:36:22 -0600

    Update mi-firma.php

--- mi-firma.php
+++ mi-firma.php
@@ -220,32 +220,59 @@
               </div>
             </div>
           </div>
-          <?php include('inc/footer.php') ?>
+  <!-- begin:: Footer -->
+  <!-- begin:: Footer -->
+  <div class="kt-footer  kt-footer--extended  kt-grid__item" id="kt_footer">
+    <div class="kt-footer__bottom">
+      <div class="kt-container">
+        <div class="kt-footer__wrapper">
+          <div class="kt-footer__logo">
+            <div class="kt-footer__copyright">
+              2019&nbsp;&copy;&nbsp;
+              <a style="color: #ffb822;" href="#">Grupo Protexa</a>
+            </div>
+
+          </div>
+          <div class="kt-footer__menu">
+            <a href="https://protexa.com.mx">Sitio Protexa</a>
+          </div>
         </div>
       </div>
     </div>
-    <!-- end:: Page -->
-    <?php include('inc/quick-panel.php') ?>
-    <!-- begin::Scrolltop -->
-    <div id="kt_scrolltop" class="kt-scrolltop">
-      <i class="fa fa-arrow-up"></i>
-    </div>
-    <!-- end::Scrolltop -->
-    <!--begin:: Global Mandatory Vendors -->
-    <?php include('inc/mansoryjs.php') ?>
-    <!--end:: Global Mandatory Vendors -->
-    <?php include('inc/global-js.php') ?>
-    <!--begin::Global Theme Bundle(used by all pages) -->
-    <script src="./assets/js/scripts.bundle.js" type="text/javascript"></script>
-    <!--end::Global Theme Bundle -->
-    <!--begin::Page Vendors(used by this page) -->
-    <script src="./assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
-    <script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM" type="text/javascript"></script>
-    <script src="./assets/vendors/custom/gmaps/gmaps.js" type="text/javascript"></script>
-    <!--end::Page Vendors -->
-    <!--begin::Page Scripts(used by this page) -->
-    <script src="./assets/js/pages/dashboard.js" type="text/javascript"></script>
-    <!--end::Page Scripts -->
+  </div>
+  <!-- end:: Footer -->
+  <!-- end:: Footer -->     
+  </div>
+  <!-- end:: Page -->
+  <!-- begin::Scrolltop -->
+  <div id="kt_scrolltop" class="kt-scrolltop">
+  <i class="fa fa-arrow-up"></i>
+  </div>
+  <!-- end::Scrolltop -->
+  <!-- begin::Global Config(global config for global JS sciprts) -->
+  <!-- end::Global Config -->
+  <!--begin:: Global Mandatory Vendors -->
+
+  <!--end:: Global Mandatory Vendors -->
+
+  <!--end:: Global Optional Vendors -->
+  <!--begin::Global Theme Bundle(used by all pages) -->
+  <script src="./assets/js/scripts.bundle.js" type="text/javascript"></script>
+  <!--end::Global Theme Bundle -->
+  <!--begin::Page Vendors(used by this page) -->
+  <script src="./assets/vendors/custom/datatables/datatables.bundle.js" type="text/javascript"></script>
+  <!--end::Page Vendors -->
+  <!--begin::Page Vendors(used by this page) -->
+  <script src="./assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+  <script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM" type="text/javascript"></script>
+  <script src="./assets/vendors/custom/gmaps/gmaps.js" type="text/javascript"></script>
+  <!--end::Page Vendors -->
+  <!--begin::Page Scripts(used by this page) -->
+  <script src="./assets/js/pages/crud/datatables/basic/basic.js" type="text/javascript"></script>
+  <!--end::Page Scripts -->
+  <!--begin::Page Scripts(used by this page) -->
+  <script src="./assets/js/pages/dashboard.js" type="text/javascript"></script>
+  <!--end::Page Scripts -->
   </body>
   <!-- end::Body -->
-  </html>
+</html>
