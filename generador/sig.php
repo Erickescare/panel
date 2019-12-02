284b8121f111edc24edcd273615ba5b5f7667680
Erickescare <erickdaniel.escareo@gmail.com>
2019-12-02 15:57:51 -0600

    Update sig.php

--- generador/sig.php
+++ generador/sig.php
@@ -2,14 +2,14 @@
 <body>
 
 <div><p style="line-height: 12px;font-size: 9pt;font-family: Helvetica,sans-serif;color: rgb(33,33,33);"><b><?php echo $_POST["fullname"]; ?></b><br />
-<img src="http://protexa-web.herokuapp.com/images/firma/lineamail.jpg" /><br />
+<img src="https://lh4.googleusercontent.com/w2NfqNquqkSGNoLZOG78w5HazUEY8TPqQxujskTJN6OYx6jMe2b1UGm6kw41LMBgQN29G70XjPKr5xFBToaCOsCAbszOKhR5wZUUYAag_sBV1k_Ar70=w1280" height="3px" width="395px" /><br />
 <span><?php echo $_POST["title"]; ?></span><br />
 <span>
 <b>Teléfono</b> <?php echo $_POST["mobile"]; ?> <b>Ext.</b> <?php echo $_POST["ext"]; ?><br />
 <!-- <b>Email</b> <a href="mailto:<?php echo $_POST["email"] ?>" style="text-decoration: none;color: rgb(33,33,33); "><?php echo $_POST["email"]; ?></a> <br /> -->
 </span>
 </p>
-<p><img src="https://www.protexa.com.mx/wp-content/uploads/2018/07/LogoWebProtexa.png" width="175px" />
+<p><img src="https://lh6.googleusercontent.com/uNvMeOxbPq_bpcg_R_geQvQGhktaM-utKvuY6_X2bvRYMwBRn1bAIVzjYSaycDcBYUv-yfGqzviIxcu6EKoFqz9EnOu2e-cTeDntBnoDLRZtUEp1xw=w1280" width="175px" />
 </p>
 <p style="line-height: 12px;font-size: 7.5pt;font-family: Arial,sans-serif;color: rgb(127,127,127);">AVISO DE CONFIDENCIALIDAD<br>
 Este correo electrónico es confidencial y para ser leído y utilizado exclusivamente por la(s) persona(s) a quien(es) se dirige. Si el lector de esta transmisión electrónica no es el destinatario, se le notifica que cualquier distribución o copia de la misma está estrictamente prohibida. Si ha recibido este correo por error le suplicamos notificar inmediatamente a la persona que lo envió y borrarlo definitivamente de su sistema.</p>
