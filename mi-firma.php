9d40086054b7c3fa284622ad6c9445c36235fc2c
Erickescare <erickdaniel.escareo@gmail.com>
2019-12-02 16:27:09 -0600

    Sbrs

--- mi-firma.php
+++ mi-firma.php
@@ -1,8 +1,88 @@
-<?php  $PageTitleMaster = 'Mi Firma'; $pageTitle = 'Mis Servicios'; include('inc/head.php') ?>
+<!DOCTYPE html>
+<html lang="mx">
+  <head>
+    <base href="./">
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+
+    <title>Encuesta de Servicio | Panel Protexa</title>
+    <meta name="description" content="Panel Protexa">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="google-signin-client_id" content="610188305605-pi5hc2lvbpg843c4tfk0iitjk64ldqtb.apps.googleusercontent.com">
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+    <script>
+      WebFont.load({
+        google: {
+          "families": ["Poppins:300,400,500,600,700", "Roboto:300,400,500,600,700"]
+        },
+        active: function() {
+          sessionStorage.fonts = true;
+        }
+      });
+    </script>
+    
+    <link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.css">
+    <link href="./assets/vendors/general/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" type="text/css" />
+
+    <link href="./assets/vendors/general/bootstrap-select/dist/css/bootstrap-select.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/select2/dist/css/select2.css" rel="stylesheet" type="text/css" />
+    
+    <link href="./assets/vendors/general/animate.css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/toastr/build/toastr.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/morris.js/morris.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/sweetalert2/dist/sweetalert2.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/socicon/css/socicon.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/custom/vendors/line-awesome/css/line-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/custom/vendors/flaticon/flaticon.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/custom/vendors/flaticon2/flaticon.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/vendors/general/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/css/pages/general/login/login-1.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" type="image/png" href="./assets/media/protexa/ico72.png" />
+    <style>
+    .disabled{
+        pointer-events:none;
+        opacity:0.7;
+    }
+    </style>
+
+    <?php 
+    $activePage = basename($_SERVER['PHP_SELF'], ".php");
+    //$pagina_anterior = basename($_SERVER['HTTP_REFERER']);
+    ?>
+  </head>
+<style>
+  /****************** Estilos Especiales Sección *****************************/
+  .kt-header__bottom{
+    display: none !important;
+  }
+</style>
   <!-- end::Head -->
   <!-- begin::Body -->
+  <link href='http://fonts.googleapis.com/css?family=Quattrocento+Sans' rel='stylesheet' type='text/css'>
+  <!-- <script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script> -->
   <body class="kt-page--loading-enabled kt-page--loading kt-page--fixed kt-quick-panel--right kt-demo-panel--right kt-offcanvas-panel--right kt-header--fixed kt-header--minimize-topbar kt-header-mobile--fixed kt-subheader--enabled kt-subheader--transparent kt-page--loading">
     <!-- begin:: Page -->
+    <!-- begin:: Header Mobile -->
+    <div id="kt_header_mobile" class="kt-header-mobile  kt-header-mobile--fixed ">
+      <div class="kt-header-mobile__logo">
+        <a href="./index.php">
+          <img alt="Logo" src="./assets/media/protexa/ico57.png" />
+        </a>
+      </div>
+      <div class="kt-header__brand kt-header__brand-nav">
+        <div class="dropdown">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" style="background-color:#ffb822;border:0;padding: 0.75rem 1.5rem;color:#ebedf2;">
+            Mi Firma
+          </button>
+        </div>
+      </div>
+      <div class="kt-header-mobile__toolbar">
+        <button class="kt-header-mobile__toolbar-toggler" id="kt_header_mobile_toggler"><span></span></button>
+        <button class="kt-header-mobile__toolbar-topbar-toggler" id="kt_header_mobile_topbar_toggler"><i class="flaticon-more-1"></i></button>
+      </div>
+    </div>
     <!-- begin:: Header Mobile -->
     <?php include('inc/header-mobile.php') ?>
     <!-- end:: Header Mobile -->
@@ -10,7 +90,29 @@
       <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
         <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper " id="kt_wrapper">
           <!-- begin:: Header -->
-          <?php include('inc/header.php') ?>
+          <div id="kt_header" class="kt-header kt-grid__item  kt-header--fixed " data-ktheader-minimize="on">
+            <div class="kt-header__top">
+              <div class="kt-container">
+                <!-- begin:: Brand -->
+                <div class="kt-header__brand   kt-grid__item" id="kt_header_brand">
+                  <div class="kt-header__brand-logo">
+                    <a href="./index.php">
+                      <img alt="Logo" src="./assets/media/protexa/LogoFB.png" width="125px" class="kt-header__brand-logo-default" />
+                      <img alt="Logo" src="./assets/media/protexa/ico72.png" width="45px" class="kt-header__brand-logo-sticky" />
+                    </a>
+                  </div>
+                  <div class="kt-header__brand-nav">
+                    <div class="dropdown">
+                      <h3 class="kt-font-warning">Mi Firma Protexa</h3>
+                    </div>
+                  </div>
+                </div>
+                <!-- end:: Brand -->
+                <!-- begin:: Header Topbar -->
+                <!-- end:: Header Topbar -->
+              </div>
+            </div>
+          </div>
           <!-- end:: Header -->
           <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-grid--stretch">
             <div class="kt-container kt-body  kt-grid kt-grid--ver" id="kt_body">
@@ -18,9 +120,9 @@
                 <!-- begin:: Content Head -->
                 <div class="kt-subheader   kt-grid__item" id="kt_subheader">
                   <div class="kt-subheader__main">
-                    <h3 class="kt-subheader__title"><?php echo $pageTitle; ?></h3>
+                    <h3 class="kt-subheader__title">Mi Firma</h3>
                     <span class="kt-subheader__separator kt-subheader__separator--v"></span>
-                    <span class="kt-subheader__desc"><?php echo $PageTitleMaster; ?></span>
+                    <span class="kt-subheader__desc">Mi Firma</span>
                     <!-- <a href="#" class="btn btn-label-primary btn-bold btn-icon-h kt-margin-l-10">
                       Más
                     </a> -->
@@ -82,7 +184,7 @@
                                 <span class="form-text text-muted">Por favor ingrese su extensión</span>
                               </div>
                             </div>
-                            <div class="form-group row">
+                            <!-- <div class="form-group row">
                               <label class="col-lg-2 col-form-label">Email:</label>
                               <div class="col-lg-3">
                                 <div class="kt-input-icon">
@@ -91,7 +193,7 @@
                                 </div>
                                 <span class="form-text text-muted">Por favor ingrese su correo electronico</span>
                               </div>
-                            </div>
+                            </div> -->
                           </div>
                           <div class="kt-portlet__foot kt-portlet__foot--fit-x">
                             <div class="kt-form__actions">
